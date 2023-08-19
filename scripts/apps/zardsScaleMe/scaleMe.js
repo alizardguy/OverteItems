a9552636d10a479d78492b324c7f53e05be36b7b
alizardguy <coyotehelp123@gmail.com>
2023-08-19 15:09:36 -0700

    Update scaleMe.js

--- scripts/apps/zardsScaleMe/scaleMe.js
+++ scripts/apps/zardsScaleMe/scaleMe.js
@@ -22,14 +22,13 @@
 
     tablet.screenChanged.connect(onScreenChanged);
 
-    // make button in tablet
     var button = tablet.addButton({
         text: APP_NAME,
         icon: APP_ICON_INACTIVE,
         activeIcon: APP_ICON_ACTIVE
     });
 
-    // function to call when tablet button is clicked
+
     function clicked(){
         if (appStatus === true) {
             tablet.webEventReceived.disconnect(onAppWebEventReceived);
@@ -49,10 +48,72 @@
 
     button.clicked.connect(clicked);
 
-    //This receives the webEvents from the Application UI.
-    function onAppWebEventReceived(message){
-        if (typeof message === "string"){
+    //This recieved the message from the UI(html) for a specific actions
+    function onAppWebEventReceived(message) {
+        if (typeof message === "string") {
             var d = new Date();
+            var n = d.getTime();
+            var instruction = JSON.parse(message);
+            if (instruction.channel === channel) {
+                if (instruction.action === "HUMAN_CALLED_ACTION_NAME" && (n - timestamp) > INTERCALL_DELAY) { //<== Use this for action trigger by a human (button or any ui control). The delay prevent multiple call to destabilize everything. 
+                    d = new Date();
+                    timestamp = d.getTime();
+                    //Call a function to do something here
+                } else if (instruction.action === "SCRIPT_CALLED_ACTION_NAME") { //<== Use this for action trigger the UI script processing. (whithout delay)
+                    //Call a function to do something here
+                } else if (instruction.action === "SELF_UNINSTALL" && (n - timestamp) > INTERCALL_DELAY) { //<== This is a good practice to add a "Uninstall this app" button for rarely used app. (toolbar has a limit in size) 
+                    d = new Date();
+                    timestamp = d.getTime();
+                    ScriptDiscoveryService.stopScript(Script.resolvePath(''), false);
+                }
+            }
         }
     }
-})
+
+    //============ Add your application functions here ==================
+
+    //Here an example of a function that would communicate data to the UI(html) for a specific action
+/*
+    function testCallingTheUItoSendData() {
+        //ADD PROCESSING HERE
+
+        var message = {
+            "channel": channel,
+            "action": "CALL_HTML_UI_ACTION_NAME",
+            "data": "Hello World!"
+        };
+
+        tablet.emitScriptEvent(JSON.stringify(message));
+    }
+*/
+    //==================================================================
+
+    function onScreenChanged(type, url) {
+        if (type === "Web" && url.indexOf(APP_URL) !== -1) {
+            appStatus = true;
+            //Here we know that the HTML UI is loaded.
+            //We could communitate to it here as we know it is loaded.
+            //testCallingTheUItoSendData();
+
+        } else {
+            appStatus = false;
+        }
+        
+        button.editProperties({
+            isActive: appStatus
+        });
+    }
+
+    function cleanup() {
+
+        if (appStatus) {
+            tablet.gotoHomeScreen();
+            tablet.webEventReceived.disconnect(onAppWebEventReceived);
+        }
+
+        tablet.screenChanged.disconnect(onScreenChanged);
+        tablet.removeButton(button);
+    }
+
+    Script.scriptEnding.connect(cleanup);
+}());
