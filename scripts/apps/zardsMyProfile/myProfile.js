7fd00ca961616d5b3cdf5c8f9ad8035107c6dc21
alizardguy <coyotehelp123@gmail.com>
2023-08-20 00:49:39 -0700

    I'm tired

--- scripts/apps/zardsMyProfile/myProfile.js
+++ scripts/apps/zardsMyProfile/myProfile.js
@@ -4,12 +4,12 @@
     var jsMainFileName = "myProfile.js"; // <=== REPLACE VALUE (File name of this current .js file)
     var ROOT = Script.resolvePath('').split(jsMainFileName)[0];
     
-    var APP_NAME = "MYPROFILE"; // <=== REPLACE VALUE (Caption of the Tablet button.)
-    var APP_URL = ROOT + "index.html"; // <=== REPLACE VALUE (html page that will be your UI)
-    var APP_ICON_INACTIVE = ROOT + "icons/inactive.png"; // <=== REPLACE VALUE (Provide a 50 X 50 pixels, .png or .svg file, WHITE on transparent background)
-    var APP_ICON_ACTIVE = ROOT + "icons/active.png"; // <=== REPLACE VALUE  (Provide a 50 X 50 pixels, .png or .svg file, BLACK on transparent background)
+    var APP_NAME = "MYPROFILE";
+    var APP_URL = ROOT + "index.html";
+    var APP_ICON_INACTIVE = ROOT + "icons/inactive.png";
+    var APP_ICON_ACTIVE = ROOT + "icons/active.png";
     var appStatus = false;
-    var channel = "overte.application.more.zardsmyprofile"; // <=== REPLACE VALUE  (This must be a string specific to this application for a private communication between the app and its UI(html). The value must the same in template.html too.)
+    var channel = "overte.application.more.zardsmyprofile"; 
     var timestamp = 0;
     var INTERCALL_DELAY = 200; //0.3 sec
     
@@ -39,49 +39,30 @@
         button.editProperties({
             isActive: appStatus
         });
+
+        sendInfoToUI();
     }
 
     button.clicked.connect(clicked);
 
-    //This recieved the message from the UI(html) for a specific actions
-    function onAppWebEventReceived(message) {
-        if (typeof message === "string") {
-            var d = new Date();
-            var n = d.getTime();
-            var instruction = JSON.parse(message);
-            if (instruction.channel === channel) {
-                if (instruction.action === "HUMAN_CALLED_ACTION_NAME" && (n - timestamp) > INTERCALL_DELAY) { //<== Use this for action trigger by a human (button or any ui control). The delay prevent multiple call to destabilize everything. 
-                    d = new Date();
-                    timestamp = d.getTime();
-                    //Call a function to do something here
-                } else if (instruction.action === "SCRIPT_CALLED_ACTION_NAME") { //<== Use this for action trigger the UI script processing. (whithout delay)
-                    //Call a function to do something here
-                } else if (instruction.action === "SELF_UNINSTALL" && (n - timestamp) > INTERCALL_DELAY) { //<== This is a good practice to add a "Uninstall this app" button for rarely used app. (toolbar has a limit in size) 
-                    d = new Date();
-                    timestamp = d.getTime();
-                    ScriptDiscoveryService.stopScript(Script.resolvePath(''), false);
-                }
-            }
-        }
-    }
+    //============ application functions ==================
 
-    //============ Add your application functions here ==================
 
-    //Here an example of a function that would communicate data to the UI(html) for a specific action
-/*
-    function testCallingTheUItoSendData() {
-        //ADD PROCESSING HERE
+    function sendInfoToUI() {
 
         var message = {
             "channel": channel,
-            "action": "CALL_HTML_UI_ACTION_NAME",
-            "data": "Hello World!"
+            "action": "GIVE_USER_INFO",
+            "username": "username"
         };
 
         tablet.emitScriptEvent(JSON.stringify(message));
     }
-*/
-    //==================================================================
+
+
+    function onAppWebEventReceived(data) {
+        // test
+    }
 
     function onScreenChanged(type, url) {
         if (type === "Web" && url.indexOf(APP_URL) !== -1) {
