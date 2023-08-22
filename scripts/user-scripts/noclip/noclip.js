17064640042d9187e2da8973138785adc59b4366
alizardguy <coyotehelp123@gmail.com>
2023-08-21 17:11:07 -0700

    Update noclip.js

--- scripts/user-scripts/noclip/noclip.js
+++ scripts/user-scripts/noclip/noclip.js
@@ -1,31 +1,16 @@
-var nocliping = false;
-
 function keyPressEvent(event) {
     if ((event.text.toUpperCase() === "V") &&
             !event.isMeta &&
             !event.isControl &&
             !event.isAlt &&
             !HMD.active) {
-                if (!nocliping){
-                    startNoclip();
-                } 
-                else {
-                    endNoclip();
-                }
+                toggleNoclip();
             }
 }
 
-
 Controller.keyPressEvent.connect(keyPressEvent);
 
-function startNoclip(){
-    MyAvatar.setCollisionsEnabled(false);
-    nocliping = true;
-    MyAvatar.isFlying = true;
-}
-
-function endNoclip(){
-    MyAvatar.setCollisionsEnabled(true);
-    nocliping = false;
-    MyAvatar.isFlying = false;
+function toggleNoclip(){
+    MyAvatar.setCollisionsEnabled(!MyAvatar.getCollisionsEnabled());
+    MyAvatar.isFlying = !MyAvatar.isFlying;
 }
