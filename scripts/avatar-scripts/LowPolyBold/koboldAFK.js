dd5ce9abb17e5e9022c593fb9d7c0f1317077041
alizardguy <coyotehelp123@gmail.com>
2023-08-23 17:19:49 -0700

    afk tracker

---
+++ scripts/avatar-scripts/LowPolyBold/koboldAFK.js
@@ -0,0 +1,7 @@
+MyAvatar.wentAway.connect(function () {
+    print("My avatar went away");
+});
+
+MyAvatar.wentActive.connect(function () {
+    print("My avatar is active");
+});
---
+++ scripts/user-scripts/tests/turnOffAvatarMeshLocal.js
@@ -0,0 +1,4 @@
+MyAvatar.setEnableMeshVisible(false);
+Script.setTimeout(function () {
+    MyAvatar.setEnableMeshVisible(true);
+}, 10000);
